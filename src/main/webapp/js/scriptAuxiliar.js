35b01752cb0be4fec5e2fa2c9a5a7295a5e648e1
daniel <danielgrafael@gmail.com>
2016-04-30 23:17:29 -0300

    Cliente + Projeto

---
+++ src/main/webapp/js/scriptAuxiliar.js
@@ -0,0 +1,32 @@
+jQuery(function ($) {
+    $("#telefone").mask("(999) 9999-9999", {placeholder: "(999) 9999-9999"});
+});
+
+$(document).ready(function () {
+    $("#adicionaNovoUsuario").click(function () {
+        $("p").show();
+    });
+    $("#CancelarAdicionarNovoUsuario").click(function () {
+        $("p").show();
+    });
+});
+
+jQuery.extend(jQuery.validator.messages, {
+    required: "Campo obrigatório!",
+    remote: "Por favor, corrija este campo.",
+    email: "Por favor, forneça um endereço eletrônico válido.",
+    url: "Por favor, forneça uma URL válida.",
+    date: "Por favor, forneça uma data válida.",
+    dateISO: "Por favor, forneça uma data válida (ISO).",
+    number: "Por favor, forneça um número válido.",
+    digits: "Por favor, forneça somente dígitos.",
+    creditcard: "Por favor, forneça um cartão de crédito válido.",
+    equalTo: "Por favor, forneça o mesmo valor novamente.",
+    accept: "Por favor, forneça um valor com uma extensão válida.",
+    maxlength: jQuery.validator.format("Por favor, forneça não mais que {0} caracteres."),
+    minlength: jQuery.validator.format("Por favor, forneça ao menos {0} caracteres."),
+    rangelength: jQuery.validator.format("Por favor, forneça um valor entre {0} e {1} caracteres de comprimento."),
+    range: jQuery.validator.format("Por favor, forneça um valor entre {0} e {1}."),
+    max: jQuery.validator.format("Por favor, forneça um valor menor ou igual a {0}."),
+    min: jQuery.validator.format("Por favor, forneça um valor maior ou igual a {0}.")
+});
